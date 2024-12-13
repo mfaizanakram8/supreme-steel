d6bb228035e9db9169ccbba72b610121f47c04f7
mfaizanakram8 <mfaizanakram536@gmail.com>
2024-12-13 19:08:34 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,19 @@ import { useEffect } from 'react';
 import AboutSection from '@/components/AboutSection';
 import HeroSection from '@/components/HeroSection';
 import InfiniteMovingCards from '@/components/InfiniteMovingCards';
-
 import ServicesSection from '@/components/ServicesSection';
 import WhyUs from '@/components/WhyUs';
-
 import MediaSection from '@/components/MediaSection';
 
 const Page: React.FC = () => {
   useEffect(() => {
+ 
+    const isDesktop = () => window.innerWidth > 768;
+
     const cursor = document.querySelector(".cursor") as HTMLElement;
 
     const updateCursorPosition = (e: MouseEvent) => {
+      if (!isDesktop()) return; // Disable for non-desktop
       const mouseX = e.clientX;
       const mouseY = e.clientY;
       cursor.style.left = `${mouseX - cursor.offsetWidth / 2}px`;
@@ -21,19 +23,21 @@ const Page: React.FC = () => {
     };
 
     const enlargeCursorOnHover = () => {
-      cursor.classList.add("cursor-hover");
+      if (isDesktop()) cursor.classList.add("cursor-hover");
     };
 
     const shrinkCursor = () => {
-      cursor.classList.remove("cursor-hover");
+      if (isDesktop()) cursor.classList.remove("cursor-hover");
     };
 
-    document.addEventListener("mousemove", updateCursorPosition);
-    const hoverElements = document.querySelectorAll(".hover-effect");
-    hoverElements.forEach((el) => {
-      el.addEventListener("mouseenter", enlargeCursorOnHover);
-      el.addEventListener("mouseleave", shrinkCursor);
-    });
+    if (isDesktop()) {
+      document.addEventListener("mousemove", updateCursorPosition);
+      const hoverElements = document.querySelectorAll(".hover-effect");
+      hoverElements.forEach((el) => {
+        el.addEventListener("mouseenter", enlargeCursorOnHover);
+        el.addEventListener("mouseleave", shrinkCursor);
+      });
+    }
 
     return () => {
       document.removeEventListener("mousemove", updateCursorPosition);
@@ -42,15 +46,15 @@ const Page: React.FC = () => {
 
   return (
     <div>
-      <div className="cursor"></div> 
-    
+      {/* Custom cursor */}
+      <div className="cursor hidden md:block"></div>
+
       <HeroSection />
       <InfiniteMovingCards />
       <ServicesSection />
       <AboutSection />
       <WhyUs />
       <MediaSection />
-
     </div>
   );
 };
